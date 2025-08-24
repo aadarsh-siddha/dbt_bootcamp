d6d620b27007915b725cd18e2a868a83d2804bca
Aadarsh Siddha <aasiddha28@gmail.com>
2025-08-24 03:04:05 +0000

    sources done

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -5,17 +5,21 @@ with customers as (
 orders as (
     select * from {{ ref("stg_jaffle_shop__orders") }}
 ),
-
+payments as (
+    select * from {{ ref("stg_stripe__payments") }}
+),
 customer_orders as (
 
     select
         customer_id,
+        min(o.order_date) as first_order_date,
+        max(o.order_date) as most_recent_order_date,
+        count(o.order_id) as number_of_orders,
+        sum(case when p.status = 'success' then p.amount else 0 end) as lifetime_value
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
-    from orders
+    from orders o
+    join payments p
+    on o.order_id = p.order_id
 
     group by 1
 
@@ -29,7 +33,8 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
 
     from customers
 
