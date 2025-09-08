084bd960a795ba58af3272abafa1e0345c3b9d62
Aadarsh Siddha <aasiddha28@gmail.com>
2025-09-08 16:28:12 +0000

    made changes

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,6 +1,9 @@
 select
     id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
+    customer as customer_id,
+    ordered_at as order_date,
+    store_id,
+    subtotal/100 as subtotal,
+    tax_paid/100 as tax_paid,
+    order_total/100 as order_total,
 from {{ source('jaffle_shop', 'orders') }}
