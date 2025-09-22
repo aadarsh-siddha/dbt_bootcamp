a7c229d683bbbdb0322bc3fb550dd14e08d06c62
Aadarsh Siddha <aasiddha28@gmail.com>
2025-09-22 19:09:15 +0000

    abcd

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -5,5 +5,5 @@ select
     store_id,
     subtotal/100 as subtotal,
     tax_paid/100 as tax_paid,
-    order_total/100 as order_total,
+    order_total/100 as order_total
 from {{ source('jaffle_shop', 'orders') }}
