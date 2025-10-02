c623fdfb73f3a25c2f2b06356feb47457497a796
Aadarsh Siddha <aasiddha28@gmail.com>
2025-10-02 21:13:54 +0000

    abcdwxyz

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -2,7 +2,7 @@ select
     id as order_id,
     customer as customer_id,
     ordered_at as order_date,
-    store_id,
+    store_id as location_id,
     subtotal/100 as subtotal,
     tax_paid/100 as tax_paid,
     order_total/100 as order_total
